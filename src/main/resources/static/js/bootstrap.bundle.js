596bdb94c9f5814158f141fef64837b2dbe65785
Farias De Sousa <mafariass@indracompany.com>
2020-07-03 10:55:36 -0300

    Teste de buiding Marcio

--- src/main/resources/static/js/bootstrap.bundle.js
+++ src/main/resources/static/js/bootstrap.bundle.js
@@ -11,7 +11,7 @@
 
   $ = $ && $.hasOwnProperty('default') ? $['default'] : $;
 
-  function _defineProperties(target, props) {
+  function _defineProperties(target, props) //{
     for (var i = 0; i < props.length; i++) {
       var descriptor = props[i];
       descriptor.enumerable = descriptor.enumerable || false;
